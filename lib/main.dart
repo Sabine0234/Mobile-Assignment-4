25944dcb8814682f0ea4064a1ba7180389c6ddf5
Sabine0234 <sabine0234@gmail.com>
2024-07-11 18:41:59 +0300

    Second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,295 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:contacts_service/contacts_service.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class _MyAppState extends State<MyApp> {
+  Locale _locale = Locale('en', '');
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  void _setLocale(Locale locale) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _locale = locale;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    return MaterialApp(
+      locale: _locale,
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en', ''), // English
+        Locale('es', ''), // Spanish
+      ],
+      home: MyHomePage(onLocaleChange: _setLocale),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final Function(Locale) onLocaleChange;
+
+  MyHomePage({required this.onLocaleChange});
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<Contact> _contacts = []; // Initialize _contacts list
+
+  @override
+  void initState() {
+    super.initState();
+    requestPermissions().then((_) {
+      getContacts();
+    });
+  }
+
+  Future<void> requestPermissions() async {
+    await [
+      Permission.contacts,
+      Permission.camera,
+      Permission.storage,
+    ].request();
+  }
+
+  Future<void> getContacts() async {
+    if (await Permission.contacts.isGranted) {
+      try {
+        Iterable<Contact> contacts = await ContactsService.getContacts();
+        setState(() {
+          _contacts = contacts.toList(); // Update _contacts list
+        });
+        for (var contact in contacts) {
+          print(contact.displayName);
+        }
+      } catch (e) {
+        print('Error fetching contacts: $e');
+      }
+    }
+  }
+
+  Future<void> pickImage(ImageSource source) async {
+    final pickedFile = await ImagePicker().pickImage(source: source);
+    if (pickedFile != null) {
+      // Handle the picked image
+      print('Picked image: ${pickedFile.path}');
+    }
+  }
+
+  Widget _buildContactItem(Contact contact) {
+    return ListTile(
+      leading: const CircleAvatar(
+        backgroundImage: AssetImage('assets/images/default_profile.png'),
+        radius: 20,
+      ),
+      title: Text(contact.displayName ?? ''),
+      onTap: () {
+        // Handle contact tap if needed
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var localization = AppLocalizations.of(context);
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(localization.translate('title')),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            DrawerHeader(
+              decoration: const BoxDecoration(
+                color: Color.fromARGB(255, 2, 95, 57),
+              ),
+              child: Stack(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          // Handle edit profile photo action
+                          showModalBottomSheet(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return SafeArea(
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: <Widget>[
+                                    ListTile(
+                                      title: Text(localization.translate('select_from_gallery')),
+                                      onTap: () {
+                                        Navigator.pop(context); // Close the bottom sheet
+                                        pickImage(ImageSource.gallery); // Pick from gallery
+                                      },
+                                    ),
+                                    ListTile(
+                                      title: Text(localization.translate('take_a_photo')),
+                                      onTap: () {
+                                        Navigator.pop(context); // Close the bottom sheet
+                                        pickImage(ImageSource.camera); // Take a photo
+                                      },
+                                    ),
+                                  ],
+                                ),
+                              );
+                            },
+                          );
+                        },
+                        child: const CircleAvatar(
+                          radius: 40,
+                          backgroundImage: AssetImage('assets/images/profile_placeholder.png'), // Placeholder image
+                          // You can replace AssetImage with NetworkImage for an online image
+                        ),
+                      ),
+                      SizedBox(height: 5),
+                      const Text(
+                        'Kigeli_34',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 10,
+                        ),
+                      ),
+                      SizedBox(height: 2),
+                      const Text(
+                        'Oliverbyo34@gmail.com',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 14,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Positioned(
+                    top: 10,
+                    right: 10,
+                    child: IconButton(
+                      icon: Icon(Icons.language, color: Colors.white),
+                      onPressed: () {
+                        // Show language selection dialog
+                        showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              title: Text(localization.translate('select_language')),
+                              content: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  _buildLanguageOption(localization, 'English', 'en'),
+                                  _buildLanguageOption(localization, 'Spanish', 'es'),
+                                  // Add more languages as needed
+                                ],
+                              ),
+                            );
+                          },
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: ListView.builder(
+        itemCount: _contacts.length,
+        itemBuilder: (context, index) {
+          Contact contact = _contacts[index];
+          return _buildContactItem(contact);
+        },
+      ),
     );
+  }
+
+  Widget _buildLanguageOption(AppLocalizations localization, String languageName, String languageCode) {
+    return ListTile(
+      title: Text(languageName),
+      onTap: () {
+        Locale newLocale = Locale(languageCode, '');
+        widget.onLocaleChange(newLocale);
+        Navigator.pop(context); // Close the dialog
+      },
+    );
+  }
+}
+
+class AppLocalizations {
+  final Locale locale;
+
+  AppLocalizations(this.locale);
+
+  static AppLocalizations of(BuildContext context) {
+    return Localizations.of<AppLocalizations>(context, AppLocalizations)!;
+  }
+
+  static const LocalizationsDelegate<AppLocalizations> delegate =
+      _AppLocalizationsDelegate();
+
+  late Map<String, String> _localizedStrings;
+
+  Future<bool> load() async {
+    try {
+      String jsonString =
+          await rootBundle.loadString('assets/lang/${locale.languageCode}.json');
+      Map<String, dynamic> jsonMap = json.decode(jsonString);
+
+      _localizedStrings = jsonMap.map((key, value) {
+        return MapEntry(key, value.toString());
+      });
+    } catch (e) {
+      print('Error loading localization for ${locale.languageCode}: $e');
+      _localizedStrings = {}; // Fallback to empty map if loading fails
+    }
+
+    return true;
+  }
+
+  String translate(String key) {
+    return _localizedStrings[key] ?? '';
+  }
+}
+
+class _AppLocalizationsDelegate
+    extends LocalizationsDelegate<AppLocalizations> {
+  const _AppLocalizationsDelegate();
+
+  @override
+  bool isSupported(Locale locale) {
+    return ['en', 'es'].contains(locale.languageCode);
+  }
+
+  @override
+  Future<AppLocalizations> load(Locale locale) async {
+    AppLocalizations localizations = new AppLocalizations(locale);
+    await localizations.load();
+    return localizations;
+  }
+
+  @override
+  bool shouldReload(LocalizationsDelegate<AppLocalizations> old) {
+    return false;
   }
 }
